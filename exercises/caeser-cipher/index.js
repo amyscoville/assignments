eadc18c36cb0472dcf2cb22649c4ec4e28861cec
amyscoville <amy.scoville@gmail.com>
2017-12-21 16:59:11 -0700

    fixed Caeser Cipher

--- exercises/caeser-cipher/index.js
+++ exercises/caeser-cipher/index.js
@@ -1,14 +1,16 @@
 const ask = require('readline-sync');
 
 let text = ask.question('Enter the text you would like to encode');
-let shift = ask.question('Enter a number between 1 and 25');
+let shift = ask.question('Enter a number');
 
 let output = '';
 let regex = /[A-Z]/;
 text = text.toUpperCase();
+shift = Math.round(shift % 26);
+console.log('shift:', shift);
 
 for(let i = 0; i < text.length; i++) {
-    let newCharCode = Number(text.charCodeAt(i)) + Number(shift);
+    let newCharCode = text.charCodeAt(i) + shift;
     if(regex.test(text[i])) {
         if(newCharCode > 90) {
             let diff = newCharCode - 90;
