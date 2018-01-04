da2334222783f2b3e4fa5097456fbc0d8233f754
amyscoville <amy.scoville@gmail.com>
2018-01-04 14:02:48 -0700

    finished React Business Homepage

--- exercises/react-business-homepage/src/App/Main/Services/index.js
+++ exercises/react-business-homepage/src/App/Main/Services/index.js
@@ -4,6 +4,7 @@ import './index.css';
 function Services() {
     return (
         <div className='services-wrapper'>
+            <img className='buffet' src="https://bigapplecurry.files.wordpress.com/2012/11/istock_000019639558_medium.jpg" alt=""/>
             <h4>Services:</h4>
             <ul className='services-list'>
                 <li className='services-li'>- Catering</li>
