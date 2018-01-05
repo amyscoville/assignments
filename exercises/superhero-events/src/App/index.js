787709357ec773a1b3fba40315cce4a0fc7f0c20
amyscoville <amy.scoville@gmail.com>
2018-01-05 16:24:00 -0700

    updated superheroes json

--- exercises/superhero-events/src/App/index.js
+++ exercises/superhero-events/src/App/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import superheroes from './superheroes';
 import './index.css';
 
 class App extends Component {
     constructor(props) {
-        
+
+    }
+    handleClick() {
+
     }
 }
+
+export default App;
