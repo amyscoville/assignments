0d1f59ae6eeebbc8f20c281cf73e71a87bca5089
amyscoville <amy.scoville@gmail.com>
2018-01-05 15:35:12 -0700

    created Superheroes exercise

---
+++ exercises/superhero-events/src/App/Superhero/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+
+function Superhero(props) {
+    return (
+        <div>
+            <h1 className='name'></h1>
+            <img className='image' src="" alt=""/>
+        </div>
+    );
+}
+
+export default Superhero;
